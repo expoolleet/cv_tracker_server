a13039d5de0de3a4fab95e0b0e04be417c40106d
expool <gosha.shcherbak@mail.ru>
2025-09-27 13:53:36 +0300

    Streaming update
    Streaming now performs with hardware encoder (h264_v4l2m2m) instead of software (libx264) to remove unnecessary CPU work

--- src/ffmpeg.py
+++ src/ffmpeg.py
@@ -8,22 +8,19 @@ class StreamProtocol(Enum):
     HTTP = 0,
     UDP = 1
     
-class FFmpeg: 
-    
+class FFmpeg:   
     def __init__(self, stream_protocol: StreamProtocol, port, framerate):
         self.stream_protocol = stream_protocol
         self.framerate = framerate
         self.frametime = 1.0/framerate
         self.port = port
-        self.pipes = []
-        
+        self.pipes = []   
         
     def create_stream(self, ip, stream_size, bitrate):
         pipe = self.get_pipe(ip, stream_size, bitrate)
         self.pipes.append(pipe)
         threading.Thread(target=self.monitor_stderr, args=(pipe, (ip, self.port)), daemon=True).start()
-        return pipe
-        
+        return pipe     
 
     def get_pipe(self, ip, stream_size, bitrate):
         if self.stream_protocol == StreamProtocol.UDP:
@@ -38,11 +35,7 @@ class FFmpeg:
             '-r',f'{self.framerate}',
             '-i','-',
             '-an',
-            '-c:v','libx264',
-            '-preset','ultrafast',
-            '-tune','zerolatency',
-            '-profile:v','baseline',
-            '-x264-params',f'keyint={self.framerate}:min-keyint={self.framerate}:scenecut=0:force-cfr=1',
+            '-c:v','h264_v4l2m2m',
             '-b:v',f'{bitrate}k',
             '-maxrate',f'{bitrate}k',
             '-bufsize',f'{bitrate * 2}k',
@@ -56,7 +49,6 @@ class FFmpeg:
             '-flush_packets','1',
             '-timeout','10000000',
             f'udp://{ip}:{self.port}?pkt_size=1316',
-            #'-progress','pipe:2'
             ], stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         if self.stream_protocol == StreamProtocol.HTTP:
             return subprocess.Popen(['ffmpeg'
@@ -69,27 +61,23 @@ class FFmpeg:
             '-f','mpegts',
             f'http://{ip}:{self.port}'], stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
-
     def monitor_stderr(self, pipe, client):
         while True:
             line = pipe.stderr.readline()
             if not line:
                 print("stderr: EOF reached")
                 break
-            print(f"FFmpeg [pipe to {client[0]}]:", line.decode().strip())
-           
+            print(f"FFmpeg [pipe to {client[0]}]:", line.decode().strip())     
             
     def close_pipes(self):
         for pipe in self.pipes:
             pipe.stdin.close()
             pipe.wait()
-        
-        
+           
     def wait_for_frametime(self, start):
         elapsed_time = time.time() - start
         if elapsed_time < self.frametime:
             time.sleep(self.frametime - elapsed_time)
-
 
     def write_frame(self, frame, pipe):
         try:
